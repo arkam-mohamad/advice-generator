db6a34ecc7f17e009c159950ccda37000aec18f2
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2023-12-05 09:38:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+let title = $('.title');
+let content = $('.content');
+let dice = $('.dice');
 
+getAdvice()
+
+$(dice).click(function () {
+  getAdvice()
+});
+
+function getAdvice(){
+  $.ajax({
+    url: "https://api.adviceslip.com/advice",
+    success: function (response) {
+      let parsed = JSON.parse(response);
+      $(title).text(`Advice #${parsed.slip.id}`);
+      $(content).text(`"${parsed.slip.advice}"`);
+    }
+  });
+}
